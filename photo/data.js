c3666f5965d47fe5472872960ca4e92c2374b3b0
David Corbin <daconex@gmail.com>
2016-01-09 22:57:39 -0500

    Created js image controller not working yet

--- photo/data.js
+++ photo/data.js
@@ -12,6 +12,36 @@ var photos = [
         "title": "Bike trip 2.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
         "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip 2.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip 2.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+    },
+    {
+        "title": "Bike trip 2.",
+        "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
+        "src": "http://i.imgur.com/Lcv4XWG.jpg"
     }
 
 ];
--- photo/js/photos.js
+++ photo/js/photos.js
@@ -2,6 +2,24 @@
  * Created by David Corbin on 12/20/2015.
  */
 
+var gallery = $(".photo-cont");
+
+/*
+ * On page load, load images from data.js file.
+ */
 $(document).ready(function() {
-    alert(photos.title);
+
+	var i = 0;
+	for (i in photos) {
+		gallery.append("<div class='col-md-3 photo-wrap' aria-id='" + i + "'><img src='" + photos[i]["src"] + "'></div>");
+		i++;
+	}
+
+	$(".photo-wrap").on("click", function(){
+		var el = $(this);
+		var aria = el.attr("aria-id");
+		$(".photo-wrap[aria-id='" + aria + "']").removeClass("col-md-3");
+		$(".photo-wrap[aria-id='" + aria + "']").addClass("col-md-12");
+	});
+
 });

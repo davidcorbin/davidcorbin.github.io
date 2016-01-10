181e1708784904d603b18d21f3fd3bfaeac2d69f
David Corbin <daconex@gmail.com>
2016-01-10 17:59:12 -0500

    Added image animation and simple background logic; not ready

--- photo/js/photos.js
+++ photo/js/photos.js
@@ -4,6 +4,8 @@
 
 var gallery = $(".photo-cont");
 
+var anim_state = "closed";
+
 /*
  * On page load, load images from data.js file.
  */
@@ -18,8 +20,34 @@ $(document).ready(function() {
 	$(".photo-wrap").on("click", function(){
 		var el = $(this);
 		var aria = el.attr("aria-id");
-		$(".photo-wrap[aria-id='" + aria + "']").removeClass("col-md-3");
-		$(".photo-wrap[aria-id='" + aria + "']").addClass("col-md-12");
+		var wrap = $(".photo-wrap[aria-id='" + aria + "']");
+
+		// If image is small, open it
+		if (wrap.hasClass("col-md-3")) {
+			// If an image is already large
+			if (anim_state != "closed") {
+				closeimage(anim_state);
+			}
+			openimage(aria);
+			anim_state = aria;
+		} 
+		// If image is large, close it 
+		else if (wrap.hasClass("col-md-12")) {
+			closeimage(aria);
+		} 
+
 	});
 
 });
+
+// Function to enlarge image
+function openimage(aria) {
+	$(".photo-wrap[aria-id='" + aria + "']").removeClass("col-md-3");
+	$(".photo-wrap[aria-id='" + aria + "']").addClass("col-md-12");
+}
+
+// Function to close image
+function closeimage(aria) {
+	$(".photo-wrap[aria-id='" + aria + "']").removeClass("col-md-12");
+	$(".photo-wrap[aria-id='" + aria + "']").addClass("col-md-3");
+}

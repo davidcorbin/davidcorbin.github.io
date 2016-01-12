7a81f8eaa6951664cde52e34ee87c44bb9a800ec
David Corbin <daconex@gmail.com>
2016-01-11 23:27:33 -0500

    Placeholder images

--- photo/data.js
+++ photo/data.js
@@ -6,42 +6,42 @@ var photos = [
     {
         "title": "Bike trip.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=0&w=400&h=300"
     },
     {
         "title": "Bike trip 2.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=1&w=400&h=300"
     },
     {
         "title": "Bike trip.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=2&w=400&h=300"
     },
     {
         "title": "Bike trip 2.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=3&w=400&h=300"
     },
     {
         "title": "Bike trip.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=4&w=400&h=300"
     },
     {
         "title": "Bike trip 2.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=5&w=400&h=300"
     },
     {
         "title": "Bike trip.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=6&w=400&h=300"
     },
     {
         "title": "Bike trip 2.",
         "desc": "Enjoying an early afternoon bike trip on the Montour trail with my Boy Scout troop. Taken on GoPro Hero 3 White.",
-        "src": "http://i.imgur.com/Lcv4XWG.jpg"
+        "src": "https://placeholdit.imgix.net/~text?txtsize=33&txt=7&w=400&h=300"
     }
 
 ];

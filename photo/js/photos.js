e0c6ca673f95f7c96d13dd0adb6b1b7bfe51b586
David Corbin <daconex@gmail.com>
2016-01-12 22:44:09 -0500

    Some what working photo gallery

--- photo/js/photos.js
+++ photo/js/photos.js
@@ -22,38 +22,37 @@ $(document).ready(function() {
 		var aria = el.attr("aria-id");
 		var wrap = $(".photo-wrap[aria-id='" + aria + "']");
 
-		// If all images are closed, open clicked image
-		if (anim_state == "closed") {
-			openimage(aria);
-			//anim_state = aria;
-		}
-
-        /*
-		// If image is large, close it 
-		else if (wrap.hasClass("col-md-12")) {
-
-            if (aria == 1) {
-                $(".photo-wrap[aria-id='0']").insertBefore(".photo-wrap[aria-id='1']");
-            }
-            if (aria == 2) {
-                $(".photo-wrap[aria-id='0']").insertBefore(".photo-wrap[aria-id='3']");
-                $(".photo-wrap[aria-id='1']").insertBefore(".photo-wrap[aria-id='3']");
-            }
-
-			closeimage(aria);
-		}
-		*/
+        openimage(aria);
 
 	});
+
+    $(".bg").on("click", function(){
+        closeimage();
+    });
 
 });
 
 // Function to enlarge image
 function openimage(aria) {
-	alert("Coming soon");
+    var i = 0;
+    var link = "";
+    for (i in photos) {
+        if (i == aria) {
+            link = photos[i]["src"];
+            break;
+        }
+        i++;
+    }
+    // Make viewer display block
+    $(".viewer").addClass("viewer-fade");
+    // Update image source
+    $(".viewer img").attr("src", link);
+    // Darken viewer background
+    $(".viewer .bg").addClass("darken");
 }
 
 // Function to close image
-function closeimage(aria) {
-	alert("Coming soon");
+function closeimage() {
+
+	$(".viewer").removeClass("viewer-fade");
 }

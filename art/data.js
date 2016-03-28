93412ad81c1c16d6af1403a2f4fd05c41c93c0a1
David Corbin <daconex@gmail.com>
2016-03-28 13:04:38 -0400

    Day 021

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Sublime",
+        "desc": "Day 021",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!22597&authkey=!ADudegBLARdVO0o&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!22598&authkey=!AJ00Jv9m42mgHI8&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Egg",
         "desc": "Day 020",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!22585&authkey=!AKtfg_cvPBqtFuI&v=3&ithint=photo%2cjpg",

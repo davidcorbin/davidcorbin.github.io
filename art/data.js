fb05ac14a607fe407f591e345ea6de33a593c4e8
David Corbin <daconex@gmail.com>
2016-04-06 19:01:31 -0400

    Day 30

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Homecoming",
+        "desc": "Day 030",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!22685&authkey=!AKiivMXnPFjeaUk&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!22683&authkey=!AOeNyXlWPB5PJCg&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Sunbeam",
         "desc": "Day 029",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!22671&authkey=!ADsuGSjE4lmfpHU&v=3&ithint=photo%2cjpg",

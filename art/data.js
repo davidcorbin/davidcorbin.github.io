b09f3a7abb30bef049d1a222b5748a7379702dd6
David Corbin <daconex@gmail.com>
2016-04-19 20:07:22 -0400

    Day 39

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Cluster",
+        "desc": "Day 039",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23175&authkey=!AGtA4ikqCt4xpCc&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23174&authkey=!AIsrJQDdmlecewQ&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Beme Logo",
         "desc": "Day 038",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23169&authkey=!AD2ogYagK3Zmj8k&v=3&ithint=photo%2cjpg",

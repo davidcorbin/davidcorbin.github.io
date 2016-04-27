060fcb00d443b56c83c30078a499f01ab3d01202
David Corbin <daconex@gmail.com>
2016-04-26 21:11:22 -0400

    Day 46

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Trig",
+        "desc": "Day 046",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23226&authkey=!AE79fHf-WS_5lBw&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23225&authkey=!AHC5kjBZTjKVxfQ&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Redfin",
         "desc": "Day 045",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23217&authkey=!AJbw6Se0cUtvP7w&v=3&ithint=photo%2cjpg",

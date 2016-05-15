0f0c36fa60a4f31b7fecef873caa67260cb032fa
David Corbin <daconex@gmail.com>
2016-05-14 22:22:38 -0400

    Day 063

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Sustain",
+        "desc": "Day 063",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23432&authkey=!AAS9uAWMTO9MmQ0&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23430&authkey=!ANZ9oXi_N0Kk2UI&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Pillars",
         "desc": "Day 062",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23417&authkey=!AKZRFcONOhwWN7U&v=3&ithint=photo%2cjpg",

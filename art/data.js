8f471db6cedeee0ce2d355a1e13fcc90be38d3c3
David Corbin <daconex@gmail.com>
2016-05-15 23:29:54 -0400

    Day 064

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Gone Mad",
+        "desc": "Day 064",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23445&authkey=!AKMun3PRXob5Wek&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23446&authkey=!AOsWqhRJCzVA-8I&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Sustain",
         "desc": "Day 063",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23432&authkey=!AAS9uAWMTO9MmQ0&v=3&ithint=photo%2cjpg",

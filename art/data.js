d8f4b49cac0dca640ed0b0b69a36ff99cbe4b929
David Corbin <daconex@gmail.com>
2016-05-20 23:47:23 -0400

    Day 069

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Covenant III",
+        "desc": "Day 069",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23494&authkey=!AH8YwpLmFnY9UB4&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23492&authkey=!ABUneMmPAfXZBas&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Vast",
         "desc": "Day 068",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23481&authkey=!AMZDXeqhYOY6CM0&v=3&ithint=photo%2cjpg",

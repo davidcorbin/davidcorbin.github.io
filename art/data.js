71320d0a6959a843fb1115b10c7ae746faa37fc5
David Corbin <daconex@gmail.com>
2016-06-06 17:39:43 -0400

    Day 085

--- art/data.js
+++ art/data.js
@@ -4,6 +4,12 @@
 
 var photos = [
     {
+        "title": "Black Sun",
+        "desc": "Day 085",
+        "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23816&authkey=!AOwfIdWEFQuyi80&v=3&ithint=photo%2cjpg",
+        "tbn": "https://onedrive.live.com/download?resid=ADB77773C67D388!23817&authkey=!AP7P-3fL5ZHWeYQ&v=3&ithint=photo%2cjpg"
+    },
+    {
         "title": "Field",
         "desc": "Day 084",
         "src": "https://onedrive.live.com/download?resid=ADB77773C67D388!23797&authkey=!ANE6lw2eG80faB4&v=3&ithint=photo%2cjpg",

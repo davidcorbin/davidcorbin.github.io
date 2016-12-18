a8d8330f95c23a13aa99213b5eba961a80db2843
David Corbin <daconex@gmail.com>
2016-12-17 21:18:51 -0500

    Fixed chrome text shadow bug

--- js/typewrite.js
+++ js/typewrite.js
@@ -1,34 +1,34 @@
 (function () {
     "use strict";
     var current = 0;
-    var tips = [
-        "Engineer, Programmer, Musician",
-        "\u2764\uFE0F Sublime Text",
-        "\u2764\uFE0F Vim",
-        "\u2764\uFE0F Github",
-        "\u2764\uFE0F Cloudflare",
-        "\u2764\uFE0F LaTeX",
-        "\u2764\uFE0F Computer Science",
-        "\u2764\uFE0F Dribbble",
-        "\u2764\uFE0F Slack",
-        "\u2764\uFE0F Travis CI",
-        "\u2764\uFE0F Jenkins CI",
-        "\u2764\uFE0F Vim",
-        "\u2764\uFE0F Project Euler",
-        "\u2764\uFE0F Stack Overflow"
+    var str = [
+        "<span>Engineer, Programmer, Musician</span>",
+        "\u2764\uFE0F <span>Sublime Text</span>",
+        "\u2764\uFE0F <span>Vim</span>",
+        "\u2764\uFE0F <span>Github</span>",
+        "\u2764\uFE0F <span>Cloudflare</span>",
+        "\u2764\uFE0F <span>LaTeX</span>",
+        "\u2764\uFE0F <span>Computer Science</span>",
+        "\u2764\uFE0F <span>Dribbble</span>",
+        "\u2764\uFE0F <span>Slack</span>",
+        "\u2764\uFE0F <span>Travis CI</span>",
+        "\u2764\uFE0F <span>Jenkins CI</span>",
+        "\u2764\uFE0F <span>Vim</span>",
+        "\u2764\uFE0F <span>Project Euler</span>",
+        "\u2764\uFE0F <span>Stack Overflow</span>"
     ];
 
     function updateText () {
-        var i = Math.floor(Math.random() * tips.length);
+        var i = Math.floor(Math.random() * str.length);
         if (current !== i) {
             $(".intro-text").removeClass("vis");
             setTimeout(function () {
-                $(".intro-text").html(tips[i]);
+                $(".intro-text").html(str[i]);
                 $(".intro-text").addClass("vis");
             }, 1000);
             current = i;
         }
     }
 
-    setInterval(updateText, 5000);
+    setInterval(updateText, 6000);
 }());
